b760d177c1d55cedced9a821891e42189927670d
AN-NI HUANG <118084497+Anny-Huang@users.noreply.github.com>
2025-05-20 16:55:04 -0600

    finish lab1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Pressable} from 'react-native';
 
 export default function App() {
+ let sampleList = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Jane' },
+  { id: 3, name: 'Doe' },
+  { id: 4, name: 'Smith' },
+ ]
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {sampleList.map((item) => (
+        <Text key={item.id} style={{ fontSize:20}}>
+          {item.name}
+        </Text>
+      ))}
+      <Pressable onPress={() => alert('I am done with the lab') }>
+        <Text style={styles.button}>Hello There</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    color: 'white',
+  }
 });
